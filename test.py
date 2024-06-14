9c6be7ce2aa70bb7dd153f153809f918f035ac58
курочкин максим <kurochkin@cmd.su>
2024-06-14 14:37:38 +0300

    test_for pull

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
-class Test():
+# class Test():
+#     def __init__(self, a: int, b: int, c: int):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#
+#     def get_umnozenie(selfs):
+#         return selfs.a * selfs.b * selfs.c
+#
+#
+# t = Test(a=1, b=2, c=3)
+# print(t.get_umnozenie())
+
+
+class test_expamle():
     def __init__(self, a: int, b: int, c: int):
         self.a = a
         self.b = b
         self.c = c
 
-    def get_umnozenie(selfs):
-        return selfs.a * selfs.b * selfs.c
+    def get_slozenie(self):
+        return self.a + self.b + self.c
 
 
-t = Test(a=1, b=2, c=3)
-print(t.get_umnozenie())
-git s
+g = test_expamle(4, 2, 3)
+
+print(g.get_slozenie())
