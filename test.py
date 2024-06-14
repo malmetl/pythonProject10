64a5c06fc9ebe2ea09c23fc2c08c0555e1f1bfdd
курочкин максим <kurochkin@cmd.su>
2024-06-14 14:28:14 +0300

    test_for pull

---
+++ test.py
@@ -0,0 +1,12 @@
+class Test():
+    def __init__(self, a: int, b: int, c: int):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_umnozenie(selfs):
+        return selfs.a * selfs.b * selfs.c
+
+
+t = Test(a=1, b=2, c=3)
+print(t.get_umnozenie())
